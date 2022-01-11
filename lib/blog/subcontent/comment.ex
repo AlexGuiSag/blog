f0ebbc76b59edc8caaa4bad488c94668a78c36ad
AlexGuiSag <aleguitronsaga@gmail.com>
2022-01-11 11:43:32 -0600

    feat: base files

---
+++ lib/blog/subcontent/comment.ex
@@ -0,0 +1,19 @@
+defmodule Blog.Subcontent.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :content, :string
+    field :name, :string
+
+    belongs_to(:post, Blog.Content.Post)
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:name, :content, :post_id])
+    |> validate_required([:name, :content, :post_id])
+  end
+end
---
+++ lib/blog_web/controllers/post_controller.ex
@@ -0,0 +1,85 @@
+defmodule BlogWeb.PostController do
+  use BlogWeb, :controller
+
+  alias Blog.Repo
+  alias Blog.Content
+  alias Blog.Content.Post
+  alias Blog.Subcontent.Comment
+
+  def index(conn, _params) do
+    posts = Content.list_posts()
+    render(conn, "index.html", posts: posts)
+  end
+
+  def new(conn, _params) do
+    changeset = Content.change_post(%Post{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def add_comment(conn, %{"comment" => comment_params, "post_id" => post_id}) do
+    post =
+      post_id
+      |> Content.get_post!()
+    #  |> Repo.preload([:comments])
+    case Content.add_comment(post_id, comment_params) do
+      {:ok, _comment} ->
+        conn
+        |> put_flash(:info, "Added comment!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+      {:error, _error} ->
+        conn
+        |> put_flash(:error, "Oops! Couldn't add comment!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+    end
+  end
+  def create(conn, %{"post" => post_params}) do
+    case Content.create_post(post_params) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post created successfully.")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    post =
+      id
+      |> Content.get_post!
+      |> Repo.preload([:comments])
+
+    changeset = Comment.changeset(%Comment{}, %{})
+    render(conn, "show.html", post: post, changeset: changeset)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    post = Content.get_post!(id)
+    changeset = Content.change_post(post)
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = Content.get_post!(id)
+
+    case Content.update_post(post, post_params) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post updated successfully.")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", post: post, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    post = Content.get_post!(id)
+    {:ok, _post} = Content.delete_post(post)
+
+    conn
+    |> put_flash(:info, "Post deleted successfully.")
+    |> redirect(to: Routes.post_path(conn, :index))
+  end
+end
